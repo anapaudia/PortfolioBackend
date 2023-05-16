a084d6f62700eace42c8efe2b75ec5c10b7c5d01
anapaudia <paulandiarena@hotmail.com>
2023-05-16 00:07:00 -0300

    Commit medianoche
    Commit de cambios, sigue habiendo error en plugin de dependency pero no encuentro el paquete necesario

---
+++ src/main/java/com/portfolio/anapaudia/Repository/RExperiencia.java
@@ -0,0 +1,12 @@
+package com.portfolio.anapaudia.Repository;
+
+import com.portfolio.anapaudia.Entity.Experiencia;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RExperiencia extends JpaRepository<Experiencia, Integer> {
+    public Optional<Experiencia> findByNombreE <String nombreE>;
+    public boolean existByNombreE (String nombreE);
+}
